116b450837deef8b6296c32cef2d2917a2e1ce3f
jvallexm <jvallexm@gmail.com>
2023-01-30 09:55:51 -0500

    fix: versions

--- versions.tf
+++ versions.tf
@@ -5,8 +5,7 @@
 terraform {
   required_providers {
   }
-  required_version = ">=1.0"
-  experiments      = [module_variable_optional_attrs]
+  required_version = ">=1.3"
 }
 
 ##############################################################################
